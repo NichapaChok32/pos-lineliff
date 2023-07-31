d618b61539f9ade85e634c67762ca5c28bf742d4
Nichapa Chokaree <nichapachok32@gmail.com>
2023-07-31 18:34:58 +0700

    add mui framework for ui

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,4 @@
+import Filter from "@/components/main/Filter";
+export default function Orders() {
+  return <Filter></Filter>;
+}
---
+++ src/components/category/ButtonGroup.tsx
@@ -0,0 +1,3 @@
+"use client";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Button from "@mui/material/Button";
