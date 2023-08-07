cb083958afd05c0e15d68d88519cf091f4663287
Nichapa Chokaree <nichapachok32@gmail.com>
2023-08-07 17:54:19 +0700

    add categories components

--- src/app/components/main/categories.tsx
+++ src/app/components/main/categories.tsx
@@ -1,9 +1,9 @@
 "use client";
 import axios from "axios";
+import { useState } from "react";
 import useSWR from "swr";
 import Image from "next/image";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
-import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
 import "../../../styles/components/Orders.scss";
 
@@ -36,38 +36,28 @@ export default function Categories() {
     "http://35.197.145.54:3000/categories",
     fetcher
   );
+  const [isSearch, setIsSearch] = useState(false);
+  function handleClick(value: boolean) {
+    setIsSearch(value);
+  }
   return (
-    <div id="Filter">
-      <Grid className="main-grid" container>
-        <Grid className="filter-grid">
-          <IconButton className="btnIcon" aria-label="search">
-            <i className="pos-search h-6 w-6 icon-filter" aria-hidden="true" />
-          </IconButton>
-        </Grid>
-        <Grid className="filter-grid">
-          <IconButton className="btnIcon" aria-label="filter">
-            <i className="pos-filter h-6 w-6 icon-filter" aria-hidden="true" />
-          </IconButton>
-        </Grid>
-        <Grid className="filter-grid categories-grid">
-          <Button className="btnCategory btnActive">
-            <Image
-              src="/all.svg"
-              alt="Category All"
-              className="all"
-              width={30}
-              height={30}
-              priority
-            />
-            All
-          </Button>
-          {data?.map((cat: Categories) => (
-            <Button className="btnCategory" key={cat.id}>
-              {cat.name}
-            </Button>
-          ))}
-        </Grid>
-      </Grid>
-    </div>
+    <Grid className="filter-grid categories-grid">
+      <Button className="btnCategory btnActive">
+        <Image
+          src="/all.svg"
+          alt="Category All"
+          className="all"
+          width={30}
+          height={30}
+          priority
+        />
+        All
+      </Button>
+      {data?.map((cat: Categories) => (
+        <Button className="btnCategory" key={cat.id}>
+          {cat.name}
+        </Button>
+      ))}
+    </Grid>
   );
 }
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,4 +1,37 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
 import Categories from "../components/main/categories";
+import "../../styles/components/Orders.scss";
+
+const token = localStorage.getItem("accessToken");
 export default function Orders() {
-  return <Categories></Categories>;
+  const [isSearch, setIsSearch] = useState(false);
+  function handleClick(value: boolean) {
+    setIsSearch(value);
+  }
+  return (
+    <div id="Filter">
+      <Grid className="main-grid" container>
+        <Grid className="filter-grid">
+          <IconButton
+            className="btnIcon"
+            aria-label="search"
+            onClick={() => handleClick(true)}
+          >
+            <i className="pos-search h-6 w-6 icon-filter" aria-hidden="true" />
+          </IconButton>
+        </Grid>
+        <Grid className="filter-grid">
+          <IconButton className="btnIcon" aria-label="filter">
+            <i className="pos-filter h-6 w-6 icon-filter" aria-hidden="true" />
+          </IconButton>
+        </Grid>
+        <Categories></Categories>
+      </Grid>
+    </div>
+  );
 }
